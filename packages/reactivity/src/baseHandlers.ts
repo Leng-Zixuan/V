53e5b925285b8368736b0611d9f29e3bb1684374
Leng <jyx13164683568@163.com>
2023-11-06 10:19:52 +0800

    构建effect函数,生成ReactEffect

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,4 +1,36 @@
+import { track, trigger } from './effect'
+
+const get = createGetter()
+
+function createGetter() {
+  return function get(target: object, key: string | symbol, receiver: object) {
+    const res = Reflect.get(target, key, receiver)
+
+    track(target, key)
+
+    return res
+  }
+}
+
+const set = createSetter()
+
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    receiver: object
+  ) {
+    const res = Reflect.set(target, key, value, receiver)
+    trigger(target, key, value)
+    return res
+  }
+}
+
 /**
  * 响应性的 handler
  */
-export const mutableHandlers: ProxyHandler<object> = {};
+export const mutableHandlers: ProxyHandler<object> = {
+  get,
+  set
+}
